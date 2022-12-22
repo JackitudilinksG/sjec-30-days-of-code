b99c7e29d6bbf25c88ca3f4f4189f3691d1bb9fc
JackitudilinksG <73828512+JackitudilinksG@users.noreply.github.com>
2022-12-22 22:10:07 +0400

    Update day-03.py

--- day-03/day-03.py
+++ day-03/day-03.py
@@ -1,1 +1,15 @@
-print("hello world")
+n = int(input("no. of numbers: "))
+
+list = []
+for x in range(1, n+1):
+    list.append(int(input("number: ")))
+
+a = 0
+for y in range(len(list)):
+	a = a + list[y]
+
+avg = a/n
+
+for z in range(len(list)):
+		if list[z] > avg:
+			print(list[z], end = " ")
