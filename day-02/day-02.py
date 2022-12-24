35c0f3536ac06fe5d081e97e836ecc54c23b3652
JackitudilinksG <73828512+JackitudilinksG@users.noreply.github.com>
2022-12-24 09:41:32 +0400

    Update day-02.py

--- day-02/day-02.py
+++ day-02/day-02.py
@@ -29,17 +29,16 @@ for x in range(1,n+1):       #adding into dictionary
     l[x] = []
     l[x].append(input("sides of triangle: "))
 
-for i in range(1):
-    key = list(l.keys())
-    for y in range(len(key)):
-        k = key.pop(0)
-        q = l.get(k)
-        while q != []:
-            m = q.pop()
-            a, b, c = m.split()
-            if k%3 == 1:
-                small(a, b, c)
-            elif k%3 == 2:
-                medium(a, b, c)
-            elif k%3 == 0:
-                big(a, b, c)
+key = list(l.keys())
+
+for y in range(len(key)):
+    k = key.pop(0)
+    q = l.get(k)
+    while q != []:
+        m = q.pop()
+        a, b, c = m.split()
+        if k%3 == 1:
+            small(a, b, c)
+        elif k%3 == 2:
+            medium(a, b, c)
+        elif k%3 == 0:
