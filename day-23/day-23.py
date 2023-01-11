1aad322f04cb954726454b34d7985a5e64fb4aba
JackitudilinksG <73828512+JackitudilinksG@users.noreply.github.com>
2023-01-11 17:16:46 +0400

    day 23 done

---
+++ day-23/day-23.py
@@ -0,0 +1,20 @@
+def revr(n):
+    
+    rev = 0
+    while(n > 0):
+        a = n % 10
+        rev = rev * 10 + a
+        n = n // 10
+    
+    return rev
+
+num = int(input("Enter a number: "))
+steps = 0
+
+while steps<10:
+    if num != revr(num):
+        num += revr(num)
+    else:
+        break
+
+print(num)
